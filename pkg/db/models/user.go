a9de59abce1529c7ddd0db39bcf69465c377b468
Luis Alfredo Ramirez Montiel <alfus@MacBook-Air-de-Lesley.local>
2024-04-03 23:11:38 -0600

    Added method to create user

--- pkg/db/models/user.go
+++ pkg/db/models/user.go
@@ -1,9 +1,28 @@
 package models
 
+import "github.com/go-pg/pg/v10"
+
 type User struct {
-	ID			string	`json:"id"`
-	FirstName	string	`json:"first_name"`
-	LastName 	string	`json:"last_name"`
-	Phone 		string	`json:"phone"`
-	Email		int32	`json:"email"`
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Phone     string `json:"phone"`
+	Email     int32  `json:"email"`
+	LoginData *LoginData `pg:"rel:has-one" json:"login_data"`
+}
+
+func CreateUser(db *pg.DB, req *User) (bool, error) {
+	_, err := db.Model(req).Insert()
+	if err != nil {
+		return false, err
+	}
+
+	user := &User{}
+
+	err = db.Model(user).
+        Relation("LoginData").
+        Where("user.id = ?", req.ID).
+        Select()
+	
+	return err != nil, err
 }
